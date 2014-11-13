57cdf1472827e2e6fdd9aaec9050a0710f34ef6f
adamdaigian <adam.daigian@gmail.com>
2014-11-12 23:36:52 -0800

    first commit

---
+++ _gh_pages/dist/js/eb.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  //mixpanel track
+  $('[track]').click(function () {
+    mixpanel.track($(this).attr("track"))  
+  });
+
+  //particles
+  $('#intro').particleground({
+    dotColor: '#154866',
+    lineColor: '#66757f'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+
+  //navbar transition
+  jQuery(function() {
+    var menu = jQuery(".navbar.navbar-default.navbar-fixed-top");
+    jQuery(window).scroll(function() {    
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 15) {
+         console.log('hello')
+         menu.addClass("navbar-fill");
+    } else {
+        menu.removeClass("navbar-fill");
+        }
+    });
+  });
+});
